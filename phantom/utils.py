5300154818a67951b61ae2c7a9e3b90a7654a997
Anton Agestam <git@antonagestam.se>
2020-10-05 11:00:15 +0200

    fix

--- phantom/utils.py
+++ phantom/utils.py
@@ -1,10 +1,5 @@
-import abc
 from typing import Optional
 from typing import Type
-
-
-def is_abstract(cls: Type) -> bool:
-    return abc.ABC in cls.__mro__
 
 
 def resolve_class_attr(cls: Type, name: str, argument: Optional[object]) -> None:
