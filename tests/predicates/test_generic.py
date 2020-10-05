33bbc47209499dedd910cd22930b46564eece764
Anton Agestam <git@antonagestam.se>
2020-10-05 12:09:41 +0200

    tests

--- tests/predicates/test_generic.py
+++ tests/predicates/test_generic.py
@@ -8,12 +8,12 @@ from phantom.predicates import generic
 
 
 class TestEqual:
-    @pytest.mark.parametrize("a,b", [(1, 1.0), (False, 0), (True, 1)])
+    @pytest.mark.parametrize("a, b", [(1, 1.0), (False, 0), (True, 1)])
     def test_returns_true_for_equal_values(self, a: object, b: object) -> None:
         assert generic.equal(a)(b) is True
         assert generic.equal(b)(a) is True
 
-    @pytest.mark.parametrize("a,b", [(2, 1), (1, 1.1), (True, False)])
+    @pytest.mark.parametrize("a, b", [(2, 1), (1, 1.1), (True, False)])
     def test_returns_false_for_non_equal_values(self, a: object, b: object) -> None:
         assert generic.equal(a)(b) is False
         assert generic.equal(b)(a) is False
@@ -21,13 +21,13 @@ class TestEqual:
 
 class TestIdentical:
     @pytest.mark.parametrize(
-        "a,b", [(1, 1), ("a", "a"), (True, True), (False, False), ((), ())]
+        "a, b", [(1, 1), ("a", "a"), (True, True), (False, False), ((), ())]
     )
     def test_returns_true_for_identical_values(self, a: object, b: object) -> None:
         assert generic.identical(a)(b) is True
         assert generic.identical(b)(a) is True
 
-    @pytest.mark.parametrize("a,b", [([], []), (1, 1.0), (False, 0), (True, 1)])
+    @pytest.mark.parametrize("a, b", [([], []), (1, 1.0), (False, 0), (True, 1)])
     def test_returns_false_for_different_values(self, a: object, b: object) -> None:
         assert generic.identical(a)(b) is False
         assert generic.identical(b)(a) is False
