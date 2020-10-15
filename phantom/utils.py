f4f429c3f0e863299d02e8fc7cf3a91b4ade4f8f
Anton Agestam <git@antonagestam.se>
2020-10-15 10:47:20 +0200

    Implement phone number types

--- phantom/utils.py
+++ phantom/utils.py
@@ -1,8 +1,41 @@
+# Workaround for something that looks like this bug
+# https://github.com/pytest-dev/pytest/issues/4386
+from __future__ import annotations
+
+import functools
+from typing import Any
+from typing import Callable
 from typing import Optional
+from typing import Tuple
 from typing import Type
+from typing import TypeVar
+from typing import Union
 
 
 def resolve_class_attr(cls: Type, name: str, argument: Optional[object]) -> None:
     argument = getattr(cls, name, None) if argument is None else argument
     if argument is not None:
         setattr(cls, name, argument)
+
+
+A = TypeVar("A")
+
+
+def excepts(
+    exception: Union[Tuple[Type[Exception], ...], Type[Exception]],
+    negate: bool = False,
+) -> Callable[[Callable[[A], Any]], Callable[[A], bool]]:
+    """Turn a unary function that raises an exception into a boolean predicate."""
+
+    def decorator(fn: Callable[[A], Any]) -> Callable[[A], bool]:
+        @functools.wraps(fn)
+        def wrapper(arg: A, /) -> bool:
+            try:
+                fn(arg)
+            except exception:
+                return negate
+            return not negate
+
+        return wrapper
+
+    return decorator
---
+++ tests/ext/test_phonenumbers.py
@@ -0,0 +1,48 @@
+import pytest
+
+from phantom.ext.phonenumbers import FormattedPhoneNumber
+from phantom.ext.phonenumbers import InvalidPhoneNumber
+from phantom.ext.phonenumbers import PhoneNumber
+
+
+class TestPhoneNumber:
+    def test_unparsable_number_is_not_instance(self):
+        assert not isinstance("+46", PhoneNumber)
+
+    def test_invalid_number_is_not_instance(self):
+        assert not isinstance("+467012345678", PhoneNumber)
+
+    def test_unformatted_number_is_instance(self):
+        assert isinstance("+46 (701) 234567", PhoneNumber)
+
+
+class TestFormattedPhoneNumber:
+    def test_unparsable_number_is_not_instance(self):
+        assert not isinstance("+46", FormattedPhoneNumber)
+
+    def test_invalid_number_is_not_instance(self):
+        assert not isinstance("+467012345678", FormattedPhoneNumber)
+
+    def test_unformatted_number_is_not_instance(self):
+        assert not isinstance("+46 (701) 234567", FormattedPhoneNumber)
+
+    def test_formatted_number_is_instance(self):
+        assert isinstance("+46701234567", FormattedPhoneNumber)
+
+    def test_normalizes_unformatted_number(self):
+        number = FormattedPhoneNumber.from_instance("+46 (701) 234567")
+        assert number == "+46701234567"
+        assert isinstance(number, FormattedPhoneNumber)
+
+    def test_from_instance_raises_for_invalid_phone_number(self):
+        with pytest.raises(InvalidPhoneNumber):
+            FormattedPhoneNumber.from_instance("+46")
+
+    def test_raises_type_error_for_out_of_bound_type(self):
+        """Since we override from_instance we need to test the bound check"""
+        value = 123
+        with pytest.raises(
+            TypeError,
+            match=fr"Can't create phantom type {FormattedPhoneNumber.__name__} from {value}",
+        ):
+            FormattedPhoneNumber.from_instance(123)
