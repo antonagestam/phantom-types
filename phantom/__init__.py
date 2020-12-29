a3277fb0b6c831fbc3119cc105ae2ce485238233
Anton Agestam <git@antonagestam.se>
2020-12-29 19:08:39 +0100

    Bump version (#67)

--- phantom/__init__.py
+++ phantom/__init__.py
@@ -3,5 +3,5 @@ from .base import PhantomBase
 from .base import Predicate
 from .base import parse_bound
 
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 __all__ = ("PhantomBase", "Phantom", "Predicate", "parse_bound")
