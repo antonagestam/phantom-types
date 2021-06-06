85abeef9773b1c61f73ff347293fd824f2763156
Anton Agestam <git@antonagestam.se>
2021-06-06 19:51:00 +0200

    add schema tests for interval types

--- tests/test_interval.py
+++ tests/test_interval.py
@@ -1,8 +1,12 @@
 import pytest
 
+from phantom.interval import Closed
+from phantom.interval import ClosedOpen
 from phantom.interval import Interval
 from phantom.interval import Natural
 from phantom.interval import NegativeInt
+from phantom.interval import Open
+from phantom.interval import OpenClosed
 from phantom.interval import Portion
 
 
@@ -12,6 +16,54 @@ class TestInterval:
 
             class I(Interval, abstract=False):  # noqa: E742
                 ...
+
+
+class TestOpen:
+    def test_implements_schema(self):
+        class Op(int, Open, low=0, high=100):
+            ...
+
+        assert Op.__schema__() == {
+            "minimum": 0,
+            "maximum": 100,
+            "description": "A value in the inclusive range (0, 100).",
+        }
+
+
+class TestClosed:
+    def test_implements_schema(self):
+        class C(float, Closed, low=-1, high=1):
+            ...
+
+        assert C.__schema__() == {
+            "description": "A value in the exclusive range [-1, 1].",
+            "exclusiveMinimum": -1,
+            "exclusiveMaximum": 1,
+        }
+
+
+class TestOpenClosed:
+    def test_implements_schema(self):
+        class OC(float, OpenClosed, low=0, high=100):
+            ...
+
+        assert OC.__schema__() == {
+            "description": "A value in the half-open range (0, 100].",
+            "minimum": 0,
+            "exclusiveMaximum": 100,
+        }
+
+
+class TestClosedOpen:
+    def test_implements_schema(self):
+        class CO(float, ClosedOpen, low=-100, high=0):
+            ...
+
+        assert CO.__schema__() == {
+            "description": "A value in the half-open range [-100, 0).",
+            "exclusiveMinimum": -100,
+            "maximum": 0,
+        }
 
 
 parametrize_negative_ints = pytest.mark.parametrize("i", (-10, -1, -0, +0))
@@ -37,6 +89,13 @@ class TestNegativeInt:
     def test_instantiation_returns_instance(self, i):
         assert i is NegativeInt.parse(i)
 
+    def test_implements_schema(self):
+        assert NegativeInt.__schema__() == {
+            "maximum": 0,
+            "minimum": None,
+            "description": "An integer value in the inclusive range (-∞, 0).",
+        }
+
 
 class TestNatural:
     @parametrize_positive_ints
@@ -59,6 +118,13 @@ class TestNatural:
     def test_instantiation_returns_instance(self, i):
         assert i is Natural.parse(i)
         assert i is Natural(i)
+
+    def test_implements_schema(self):
+        assert Natural.__schema__() == {
+            "description": "An integer value in the inclusive range (0, ∞).",
+            "minimum": 0,
+            "maximum": None,
+        }
 
 
 parametrize_portion_values = pytest.mark.parametrize(
@@ -88,3 +154,10 @@ class TestPortion:
             Portion.parse(i)
         with pytest.raises(TypeError):
             Portion(i)
+
+    def test_implements_schema(self):
+        assert Portion.__schema__() == {
+            "description": "A float value in the inclusive range (0, 1).",
+            "minimum": 0,
+            "maximum": 1,
+        }
