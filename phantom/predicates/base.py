02e5e74e13febe21a4e2b0204a018949a1c5f445
sobolevn <mail@sobolevn.me>
2021-07-07 13:41:06 +0300

    Fixes mypy

--- phantom/predicates/base.py
+++ phantom/predicates/base.py
@@ -1,10 +1,8 @@
+from typing import Callable
 from typing import TypeVar
 
 from typing_extensions import Protocol
 
 T = TypeVar("T", bound=object, contravariant=True)
 
-
-class Predicate(Protocol[T]):
-    def __call__(self, arg: T) -> bool:
-        ...
+Predicate = Callable[[T], bool]
