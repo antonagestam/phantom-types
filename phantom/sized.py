8233519f6755cc29b08e7b734c61ca486e7a3d00
sobolevn <mail@sobolevn.me>
2021-07-13 18:57:40 +0300

    Tries to solve metaclass conflict

--- phantom/sized.py
+++ phantom/sized.py
@@ -22,9 +22,17 @@ from typing import MutableSet
 from typing import Sized
 from typing import TypeVar
 
+# We attempt to import _ProtocolMeta from typing_extensions to support Python 3.7 but
+# fall back the typing module to support Python 3.8+. This is the closest I could find
+# to documentation of _ProtocolMeta.
+# https://github.com/python/cpython/commit/74d7f76e2c953fbfdb7ce01b7319d91d471cc5ef
+try:
+    from typing_extensions import _ProtocolMeta  # type: ignore[attr-defined]
+except ImportError:
+    from typing import _ProtocolMeta  # type: ignore[attr-defined]
+
 from typing_extensions import Final
 from typing_extensions import Protocol
-from typing_extensions import _ProtocolMeta  # type: ignore[attr-defined]
 from typing_extensions import runtime_checkable
 
 from . import Phantom
