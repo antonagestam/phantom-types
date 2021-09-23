63e899836edf217229f3ca63ca6f27d58f3b9cbe
Anton Agestam <git@antonagestam.se>
2021-09-23 23:32:00 +0200

    Expose and document the `excepts` decorator (#142)

--- phantom/fn.py
+++ phantom/fn.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
+
+import functools
 from functools import partial
+from typing import Any
 from typing import Callable
 from typing import TypeVar
 
@@ -31,3 +35,34 @@ def compose2(a: Callable[[AA], AR], b: Callable[[BA], AA]) -> Callable[[BA], AR]
 
     c.__name__ = f"{a_name}∘{b_name}"
     return c
+
+
+A = TypeVar("A")
+
+
+def excepts(
+    exception: tuple[type[Exception], ...] | type[Exception],
+    negate: bool = False,
+) -> Callable[[Callable[[A], Any]], Callable[[A], bool]]:
+    """
+    Turn a unary function that raises an exception into a boolean predicate.
+
+    >>> def validate_positive(number: int) -> None:
+    ...     if number < 0: raise ValueError
+    >>> is_positive = excepts(ValueError)(validate_positive)
+    >>> is_positive(0), is_positive(-1)
+    (True, False)
+    """
+
+    def decorator(fn: Callable[[A], Any]) -> Callable[[A], bool]:
+        @functools.wraps(fn)
+        def wrapper(arg: A) -> bool:
+            try:
+                fn(arg)
+            except exception:
+                return negate
+            return not negate
+
+        return wrapper
+
+    return decorator
