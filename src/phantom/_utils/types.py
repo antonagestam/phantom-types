95b9d063ae1920ce3f8af0f46693f8aad7f2f3e9
Anton Agestam <git@antonagestam.se>
2022-11-17 20:47:21 +0100

    fix work-around mypy crash

--- src/phantom/_utils/types.py
+++ src/phantom/_utils/types.py
@@ -1,14 +1,9 @@
-import sys
-from typing import TYPE_CHECKING
 from typing import TypeVar
-
-if sys.version_info >= (3, 8) or TYPE_CHECKING:
-    from numerary.protocol import CachingProtocolMeta
-else:
-    from numerary.types import CachingProtocolMeta
 
 from typing_extensions import Protocol
 from typing_extensions import runtime_checkable
+
+from .compat import CachingProtocolMeta
 
 T_contra = TypeVar("T_contra", contravariant=True)
 U_co = TypeVar("U_co", covariant=True)
